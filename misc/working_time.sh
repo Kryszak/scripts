2509e74f48f9f91009153d3997a40db15c580e6d
Kryszak <prajskrzysztof@gmail.com>
2020-05-02 13:38:32 +0200

    correct working_time.sh

--- misc/working_time.sh
+++ misc/working_time.sh
@@ -2,4 +2,4 @@
 
 # Get sum of hours from last 5 days
 
-last -da | grep $USER | tail -n +2 | head -n 5 | tr -s " " | awk '{print $9}' | tr -d '()' | awk -F ':' '{hours+=$1; minutes+=$2} END {time=hours+minutes/60; print time "h"}'
+last | grep $USER | tail -n +2 | head -n 5 | tr -s " " | awk '{print $9}' | tr -d '()' | awk -F ':' '{hours+=$1; minutes+=$2} END {time=hours+minutes/60; print time "h"}'
