ebfe0b46a716d4cc0ddd95e3c4e5898626712a43
Kryszak <prajskrzysztof@gmail.com>
2021-05-08 15:00:43 +0200

    adjust cleaning script

--- manjaro/cleaning.sh
+++ manjaro/cleaning.sh
@@ -2,7 +2,9 @@
 
 # This script cleans disk space on Manjaro Linux
 
-set -ex
+CYAN='\033[0;36m'
+NC='\033[0m'
+
 echo "Cleaning up journal logs"
 journalctl --disk-usage
 sudo journalctl --vacuum-size=50M
@@ -15,5 +17,6 @@ echo "Cleaning up spotify cache"
 rm -rf ~/.cache/spotify/Storage/*
 echo "Cleaning up yay cache"
 yay -Sc -a
-echo "Packages not used by any other package:"
+echo -e "${CYAN}Packages not used by any other package:${NC}"
+echo ""
 pacman -Qdt
