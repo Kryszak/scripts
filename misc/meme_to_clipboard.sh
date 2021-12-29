a9b569cdad86a3dfc4fb8e8513d071935a7916a5
Kryszak <prajskrzysztof@gmail.com>
2021-12-29 11:18:16 +0100

    migrate meme making script to shitpost

--- misc/meme_to_clipboard.sh
+++ misc/meme_to_clipboard.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 INPUT_IMAGE=$1
-INPUT_TEXT=$2
+TOP_TEXT=$2
+BOTTOM_TEXT=$3
 
-MEME_PATH=$(meme -i "${INPUT_IMAGE}" -t "${INPUT_TEXT}")
+MEME_PATH=${HOME}/meme.png
+
+shitpost -f "${INPUT_IMAGE}" -t "${TOP_TEXT}" -b "${BOTTOM_TEXT}" -o $MEME_PATH
 
 xclip -selection clipboard -t image/png -i $MEME_PATH
+
+rm $MEME_PATH
