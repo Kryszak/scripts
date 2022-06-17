58b3c44c78fec5e6c7c07968e4f0a53ab8c9aa68
Kryszak <prajskrzysztof@gmail.com>
2022-06-17 23:50:36 +0200

    replace yay cleaning with paru cleaning

--- system/cleaning.sh
+++ system/cleaning.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script cleans disk space on Manjaro Linux
+# This script cleans disk space on Arch Linux
 
 CYAN='\033[0;36m'
 NC='\033[0m'
@@ -15,8 +15,8 @@ echo "Cleaning up systemd coredumps"
 sudo rm -f /var/lib/systemd/coredump/*
 echo "Cleaning up spotify cache"
 rm -rf ~/.cache/spotify/Storage/*
-echo "Cleaning up yay cache"
-yay -Sc -a
+echo "Cleaning up paru cache"
+paru -Sc -a
 echo -e "${CYAN}Packages not used by any other package:${NC}"
 echo ""
 pacman -Qdt || true
