602bdff0381dc1895085a3a609037c630afe79a9
Kryszak <prajskrzysztof@gmail.com>
2023-04-03 18:55:33 +0200

    add solaar script to set logitech mx master 3 mouse and mx keys keyboard to correct layouts

---
+++ misc/logitech_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# set keyboard layout to Win/Linux
+solaar config "MX Keys Keyboard" multiplatform Windows
+
+# set correct mouse scroll direction
+solaar config "MX Master 3 Wireless Mouse" hires-smooth-invert false
