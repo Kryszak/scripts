64a27c9bd8fbdc5d260d21d054305e90493d347d
Kryszak <prajskrzysztof@gmail.com>
2023-04-19 18:15:00 +0200

    update grub install script

--- system/install_grub.sh
+++ system/install_grub.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+echo "Installing grub"
 sudo grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+echo "Generating new grub configuration"
+sudo grub-mkconfig -o /boot/grub/grub.cfg
