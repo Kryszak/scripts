8af65932aba3692a9ee69d99e0f29e430afb0680
Kryszak <prajskrzysztof@gmail.com>
2023-06-04 21:03:48 +0200

    add cargo registry cleanup

--- system/cleaning.sh
+++ system/cleaning.sh
@@ -17,6 +17,8 @@ echo "Cleaning up spotify cache"
 rm -rf ~/.cache/spotify/Storage/*
 echo "Cleaning up paru cache"
 paru -Sc -a
+echo "Cleaning up cargo cache"
+rm -rf ~/.cargo/registry/*
 echo -e "${CYAN}Packages not used by any other package:${NC}"
 echo ""
 pacman -Qdt || true
