f977ac4bd5d58c01f80d249de03ef677a197dcbc
Kryszak <prajskrzysztof@gmail.com>
2023-09-08 20:07:19 +0200

    improve system backup script to remove oldest backup

--- misc/backup_system.sh
+++ misc/backup_system.sh
@@ -1,3 +1,6 @@
 
 #!/bin/bash
+echo "Creating new system snapshot"
 sudo timeshift --create
+echo "Clearing oldest snapshot"
+sudo timeshift --delete --snapshot $(sudo timeshift --list | grep "0    >" | grep -Eo "[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}")
