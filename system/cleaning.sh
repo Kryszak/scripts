d05b27fde372c7d9dd44b6368fe18abf27c91e57
Kryszak <prajskrzysztof@gmail.com>
2023-10-05 17:56:03 +0200

    update cleaning script for paru, add script for merging pacdiffs

--- system/cleaning.sh
+++ system/cleaning.sh
@@ -8,15 +8,13 @@ NC='\033[0m'
 echo "Cleaning up journal logs"
 journalctl --disk-usage
 sudo journalctl --vacuum-size=50M
-echo "Cleaning up pacman caches"
-sudo pacman -Scc
+echo "Cleaning up paru caches"
+sudo paru -Scc
 sudo paccache -rvuk0
 echo "Cleaning up systemd coredumps"
 sudo rm -f /var/lib/systemd/coredump/*
 echo "Cleaning up spotify cache"
 rm -rf ~/.cache/spotify/Storage/*
-echo "Cleaning up paru cache"
-paru -Sc -a
 echo "Cleaning up cargo cache"
 rm -rf ~/.cargo/registry/*
 echo -e "${CYAN}Packages not used by any other package:${NC}"
