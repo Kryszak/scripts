a0c4a937b82a0e75d9d48c6ba1440823cd1cf6a3
Kryszak <prajskrzysztof@gmail.com>
2024-10-16 22:15:25 +0200

    update cleaning script

--- system/cleaning.sh
+++ system/cleaning.sh
@@ -9,12 +9,12 @@ echo "Cleaning up journal logs"
 journalctl --disk-usage
 sudo journalctl --vacuum-size=50M
 echo "Cleaning up paru caches"
-sudo paru -Scc
+sudo paru -Sccd
 sudo paccache -rvuk0
 echo "Cleaning up systemd coredumps"
 sudo rm -f /var/lib/systemd/coredump/*
 echo "Cleaning up spotify cache"
-rm -rf ~/.cache/spotify/Storage/*
+rm -rf ~/.cache/spotify/*
 echo "Cleaning up cargo cache"
 rm -rf ~/.cargo/registry/*
 echo -e "${CYAN}Packages not used by any other package:${NC}"
