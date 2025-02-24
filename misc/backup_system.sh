7eaffb2edf597141a6c3caa7e3e3d4e854cb81c8
Kryszak <prajskrzysztof@gmail.com>
2025-02-24 21:00:51 +0100

    fixed timeshift errors

--- misc/backup_system.sh
+++ misc/backup_system.sh
@@ -9,15 +9,15 @@ echo -e "${BOLD}Backing up user files${NC}"
 ./rsync_to_pendrive.sh
 
 echo -e "${BOLD}Creating new system snapshot${NC}"
-sudo timeshift --create
+sudo env LC_ALL=C.UTF-8 timeshift --create
 
 SNAPSHOT_COUNT=$(sudo timeshift --list | awk '/------------------------------------------------------------------------------/{y=1;next}y' | grep . | wc -l)
 if [[ $SNAPSHOT_COUNT -ge $SNAPSHOT_COUNT_LIMIT ]]; then
 	echo -e "${BOLD}Clearing oldest snapshot${NC}"
-	sudo timeshift --delete --snapshot $(sudo timeshift --list | grep "0    >" | grep -Eo "[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}")
+	sudo env LC_ALL=C.UTF-8 timeshift --delete --snapshot $(sudo timeshift --list | grep "0    >" | grep -Eo "[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}")
 else
 	echo -e "${BOLD}Less than ${SNAPSHOT_COUNT_LIMIT} snapshots present, no cleanup${NC}"
 fi
 
 echo -e "${BOLD}Present snapshots${NC}"
-sudo timeshift --list
+sudo env LC_ALL=C.UTF-8 timeshift --list
