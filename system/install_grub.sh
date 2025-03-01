257d550c0dba8b02e18c96444dd62488a16ab802
Kryszak <prajskrzysztof@gmail.com>
2025-03-01 12:38:09 +0100

    fix installing grub, add prompt to not fuck up bootloader

--- system/install_grub.sh
+++ system/install_grub.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-echo "Installing grub"
-sudo grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-echo "Generating new grub configuration"
-sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+EFI_DIRECTORY="/boot"
+echo "Installing grub to $EFI_DIRECTORY"
+read -p "Are you sure this is correct location? [y/n] " -n 1 -r
+echo    
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    sudo grub-install --target=x86_64-efi --efi-directory=$EFI_DIRECTORY --bootloader-id=GRUB
+    echo "Generating new grub configuration"
+    sudo grub-mkconfig -o "$EFI_DIRECTORY/grub/grub.cfg"
+fi
+
