2dd33bca8d2f8f3e27ff11d5f1c32c6c77de0cb2
Kryszak <prajskrzysztof@gmail.com>
2025-03-01 13:53:24 +0100

    remove envs for timeshift, improve grub installation script

--- misc/backup_system.sh
+++ misc/backup_system.sh
@@ -3,21 +3,21 @@ set -e
 
 BOLD="\033[1m"
 NC="\033[0m"
-SNAPSHOT_COUNT_LIMIT=2
+DELETE_LATEST_SNAPSHOT_THRESHOLD=3
 
 echo -e "${BOLD}Backing up user files${NC}"
 ./rsync_to_pendrive.sh
 
 echo -e "${BOLD}Creating new system snapshot${NC}"
-sudo env LC_ALL=C.UTF-8 timeshift --create
+sudo timeshift --create
 
 SNAPSHOT_COUNT=$(sudo timeshift --list | awk '/------------------------------------------------------------------------------/{y=1;next}y' | grep . | wc -l)
-if [[ $SNAPSHOT_COUNT -ge $SNAPSHOT_COUNT_LIMIT ]]; then
+if [[ $SNAPSHOT_COUNT -ge $DELETE_LATEST_SNAPSHOT_THRESHOLD ]]; then
 	echo -e "${BOLD}Clearing oldest snapshot${NC}"
-	sudo env LC_ALL=C.UTF-8 timeshift --delete --snapshot $(sudo timeshift --list | grep "0    >" | grep -Eo "[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}")
+	sudo timeshift --delete --snapshot $(sudo timeshift --list | grep "0    >" | grep -Eo "[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}")
 else
-	echo -e "${BOLD}Less than ${SNAPSHOT_COUNT_LIMIT} snapshots present, no cleanup${NC}"
+	echo -e "${BOLD}Less than ${DELETE_LATEST_SNAPSHOT_THRESHOLD} snapshots present, no cleanup${NC}"
 fi
 
 echo -e "${BOLD}Present snapshots${NC}"
-sudo env LC_ALL=C.UTF-8 timeshift --list
+sudo timeshift --list
--- system/install_grub.sh
+++ system/install_grub.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+set -euo pipefail
+
 EFI_DIRECTORY="/boot"
-echo "Installing grub to $EFI_DIRECTORY"
+
+echo "Attempting grub installation to $EFI_DIRECTORY"
+echo "$EFI_DIRECTORY content:"
+ls --color=auto -l $EFI_DIRECTORY
+echo
+echo "Target directory MUST contain 'EFI' directory and 'grub' directory"
 read -p "Are you sure this is correct location? [y/n] " -n 1 -r
-echo    
+echo 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+    echo "Installing grub"
     sudo grub-install --target=x86_64-efi --efi-directory=$EFI_DIRECTORY --bootloader-id=GRUB
     echo "Generating new grub configuration"
     sudo grub-mkconfig -o "$EFI_DIRECTORY/grub/grub.cfg"
+else
+    echo "No changes were made, aborting."
 fi
 
